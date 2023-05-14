1c0d77bc48e7ee26f1d9d340c412ee9b19af3653
i3ym <deniso99@mail.ru>
2023-05-14 14:11:59 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+if (args[0] == "-d")
+{
+    Console.WriteLine(HttpUtility.UrlDecode(string.Join(' ', args[1..])));
+    return;
+}
+
+Console.WriteLine(HttpUtility.UrlEncode(string.Join(' ', args)));
