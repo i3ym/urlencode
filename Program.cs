fbed28791ed8b3bcab5f42ad366ec20dca4a147a
i3ym <deniso99@mail.ru>
2023-08-22 18:07:11 +0700

    Add support for stream input

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿using System.Web;
 
-if (args[0] == "-d")
-{
-    Console.WriteLine(HttpUtility.UrlDecode(string.Join(' ', args[1..])));
-    return;
-}
 
-Console.WriteLine(HttpUtility.UrlEncode(string.Join(' ', args)));
+if (args.Length > 0 && args[0] == "-d")
+{
+    var data = args.Length == 1
+        ? await new StreamReader(Console.OpenStandardInput()).ReadToEndAsync()
+        : "\n";
+    data = data[..^1]; // remove newline
+
+    if (data.Length != 0) Console.WriteLine(HttpUtility.UrlDecode(data));
+    if (args.Length > 1) Console.WriteLine(HttpUtility.UrlDecode(string.Join(' ', args[1..])));
+}
+else
+{
+    var data = args.Length == 0
+        ? await new StreamReader(Console.OpenStandardInput()).ReadToEndAsync()
+        : "\n";
+    data = data[..^1]; // remove newline
+
+    if (data.Length != 0) Console.WriteLine(HttpUtility.UrlEncode(data));
+    if (args.Length > 0) Console.WriteLine(HttpUtility.UrlEncode(string.Join(' ', args)));
+}
